d1b8414173ced2ec01bcc7604d7e70980bb46089
mvP4R4 <hello@runepjetursson.com>
2025-01-11 00:51:46 +0100

    init 2.0
    Module and dpendencies install

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/seo",
+    "nuxt-cloudflare-analytics",
+    "@nuxtjs/color-mode",
+  ],
+  css: ["/assets/style/main.scss"],
+});
