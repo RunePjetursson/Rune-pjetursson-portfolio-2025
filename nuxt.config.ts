4107a211914bd7097f4b56ab5f9595f0cfe2baca
mvP4R4 <hello@runepjetursson.com>
2025-01-17 01:42:58 +0100

    Fixed robots
    Disallow: /admin/
    Allow: /public/

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: [
-    "@nuxtjs/seo",
-    "nuxt-cloudflare-analytics",
-    "@nuxtjs/color-mode",
-    "@nuxtjs/google-fonts",
-  ],
+  modules: ["@nuxtjs/seo", "@nuxtjs/color-mode", "@nuxtjs/google-fonts"],
   googleFonts: {
     families: {
       Quicksand: {
